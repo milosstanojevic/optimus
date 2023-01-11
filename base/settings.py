00de04eb8756baa0de8adc80abc09318648dbcf8
Milos Stanojevic <milos.stanojevic@herohealth.com>
2023-01-11 12:35:18 +0100

    Add env for project secret key and debug

--- base/settings.py
+++ base/settings.py
@@ -28,11 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k1c_^g!2p8z4pg3at-ke4*yhhasnk3!!56hp2t$^o1_-x&3vc9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('PROJECT_SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG_KEY') == 'DEBUG'
 
 ALLOWED_HOSTS = []
 
